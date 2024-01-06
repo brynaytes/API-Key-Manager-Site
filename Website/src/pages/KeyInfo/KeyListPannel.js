89d9529a0e5e1409e005ec105207bdf0051095b6
BRYNSFRIGATE\bryna <brynaytes@gmail.com>
2024-01-06 12:46:11 -0600

    things

--- Website/src/pages/KeyInfo/KeyListPannel.js
+++ Website/src/pages/KeyInfo/KeyListPannel.js
@@ -1,5 +1,5 @@
 import './KeyInfoPage.css';
-
+import $ from 'jquery';
 
 
 
@@ -7,12 +7,13 @@ function createKeyListElements(keyNames){
 
     let temp = keyNames.map(item => {
         console.log("item",item)
-        return <input type="submit" key={item.id} value={item.name}/>
+        return <button class="list-group-item list-group-item-action" type="submit" key={item.id} value={item.name}/>
 
     });
     console.log(temp);
     return <div>{temp}</div>
 }
+
 
 /**
  * Pannel displaying api key info and search options
@@ -24,7 +25,7 @@ function createKeyListElements(keyNames){
             <input type="text" id="searchField" name="fname"/>
             <input type="submit" value="search"/>
         </div>
-        <div className='keyList'>
+        <div className='list-group' id="keyList">
             {createKeyListElements(props.list)}
         </div>
         <h1>1</h1>
